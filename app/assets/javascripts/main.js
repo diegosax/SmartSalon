89206df513f8b1f240cce11d759e9582aff84a85
Diego Henrique Monteiro Marcolan <diegosax@terra.com.br>
2012-07-23 11:30:46 -0300

    Adding ajax loading information

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,3 +1,24 @@
+function addLoading(element){
+	var html = "<div class = 'loading'>Processando, por favor aguarde...<br /><img src='/assets/ajax_loader.gif'></img>"
+	$(element).append(html).fadeIn("slow");
+}
+
+function removeLoading(){
+	$(".loading").remove().fadeOut("fast");
+}
+function loadDateTimePicker(){
+	var minu = (60 - (new Date().getMinutes()))%5;
+	$('#datetime_from').datetime({value: '+'+minu+'min',format: 'dd-mm-yy hh:ii',altField: '#datetime_from_alt', altFormat: "d 'de' MM yy 'às' hh:ii", chainTo: '#datetime_to', chainOptions: { value: '' } });
+	$('#datetime_to').datetime({format: 'dd-mm-yy hh:ii',altField: '#datetime_to_alt', altFormat: "d 'de' MM yy 'às' hh:ii"});
+	$('#datetime_atualizacao_data').datetime({value: '+1min',format: 'dd-mm-yy hh:ii',altField: '#datetime_atualizacao_data_alt', altFormat: "d 'de' MM yy 'às' hh:ii" });
+}
+
+function onChooseProfessionalChange(){
+	$("#calendar").remove().fadeOut("fast");		
+	addLoading($("#new-event-form"));
+	$("#new-event-form").submit();
+	history.pushState(null,"",$("#new-event-form").attr("action") + "?" + $("#new-event-form").serialize());
+}
 $(document).ready(function(){
 
 	//----------------------------------------------------------------------------------------
@@ -9,19 +30,14 @@ $(document).ready(function(){
 	//----------------------------------------------------------------------------------------------------
 
 	window.onpopstate = function(e) { 
-	    if (!(typeof e.state == 'undefined')) { 
+		if (!(typeof e.state == 'undefined')) { 
 	       //$.getScript(location.href);
-	    } 
+	   } 
 	}
 
-	function loadDateTimePicker(){
-        var minu = (60 - (new Date().getMinutes()))%5;
-        $('#datetime_from').datetime({value: '+'+minu+'min',format: 'dd-mm-yy hh:ii',altField: '#datetime_from_alt', altFormat: "d 'de' MM yy 'às' hh:ii", chainTo: '#datetime_to', chainOptions: { value: '' } });
-        $('#datetime_to').datetime({format: 'dd-mm-yy hh:ii',altField: '#datetime_to_alt', altFormat: "d 'de' MM yy 'às' hh:ii"});
-        $('#datetime_atualizacao_data').datetime({value: '+1min',format: 'dd-mm-yy hh:ii',altField: '#datetime_atualizacao_data_alt', altFormat: "d 'de' MM yy 'às' hh:ii" });
-    }
-    
-    loadDateTimePicker();
+	
+
+	loadDateTimePicker();
 
 
 	$(".day_number a").live("click",function(){
@@ -39,11 +55,11 @@ $(document).ready(function(){
 			hideOthers: true,
 			showOn: 'click',
 			spinner: {
-  				radii:     [32, 42],
-  				color:     '#808080',
-  				dashWidth: 1,
-  				dashes:    75,
-  				speed:     .7
+				radii:     [32, 42],
+				color:     '#808080',
+				dashWidth: 1,
+				dashes:    75,
+				speed:     .7
 			},
 			hook: {
 				target: 'rightmiddle',
@@ -57,6 +73,11 @@ $(document).ready(function(){
 		event.preventDefault();
 	});
 
+	$("a[data-remote=true]").live("click",function(event){
+		$(this).closest("div").remove().fadeOut("fast");
+		addLoading($("new-event-form"));
+	});
+
 	$("#newEvent").on('shown',function(){
 		var now = new Date();
 		var minute = now.getMinutes();
@@ -66,20 +87,21 @@ $(document).ready(function(){
 		var hora = now.getHours() + ":" + minute;
 		var date = document.URL.split('#')[1] + " " + hora;
 		
-        $('#datetime_from').datetime({value: date,format: 'dd-mm-yy hh:ii',altField: '#datetime_from_alt', altFormat: "d 'de' MM yy 'às' hh:ii", chainTo: '#datetime_to', chainOptions: { value: '' } });
-        $('#datetime_to').datetime({format: 'dd-mm-yy hh:ii',altField: '#datetime_to_alt', altFormat: "d 'de' MM yy 'às' hh:ii", });
+		$('#datetime_from').datetime({value: date,format: 'dd-mm-yy hh:ii',altField: '#datetime_from_alt', altFormat: "d 'de' MM yy 'às' hh:ii", chainTo: '#datetime_to', chainOptions: { value: '' } });
+		$('#datetime_to').datetime({format: 'dd-mm-yy hh:ii',altField: '#datetime_to_alt', altFormat: "d 'de' MM yy 'às' hh:ii", });
 	});	
 
 	$("#choose-service .chzn_a").chosen().change(function(){
+		$("#calendar").remove().fadeOut("fast");
+		$("#choose-professional").remove().fadeOut("fast");
+		addLoading($("#new-event-form"));
 		$("#choose-professional select option[value='']").attr('selected',true);
 		$("#new-event-form").submit();
 		history.pushState(null,"",$("#new-event-form").attr("action") + "?" + $("#new-event-form").serialize());
 	});
 
 	$("#choose-professional .chzn_a").chosen().change(function(){
-		//$("#choose-service select option[value='']").attr('selected',true);
-		$("#new-event-form").submit();
-		history.pushState(null,"",$("#new-event-form").attr("action") + "?" + $("#new-event-form").serialize());
+		onChooseProfessionalChange();
 	});
 
 	$("#new-event-form").on("submit",function(){
