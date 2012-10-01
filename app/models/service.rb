85cbb92f5bfb090db40821c11fa038f115e82f21
rafael <rafael@rafael-VirtualBox.(none)>
2012-10-01 13:24:33 -0300

    basic validation

--- app/models/service.rb
+++ app/models/service.rb
@@ -4,4 +4,6 @@ class Service < ActiveRecord::Base
   has_many :professionals, :through => :professional_services
   has_many :events
   belongs_to :salon
+  validates :name, :duration, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
 end
