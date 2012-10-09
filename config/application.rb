7ad23707cdf6b97970b8f0913428d1f33bc7d5db
Diego Henrique Monteiro Marcolan <diegosax@terra.com.br>
2012-10-09 15:50:17 -0300

    Fixed login page

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,7 @@ module Smartsalon
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = "pt-BR"
     config.autoload_paths += %W[#{config.root}/lib/validators/]
     config.autoload_paths += %W[#{config.root}/lib/modules/]
 
