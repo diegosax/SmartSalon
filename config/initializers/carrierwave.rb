cabb80a15a979ede9f0d30ff94d4dbbd0f421a32
Diego Marcolan <diegosax@terra.com.br>
2012-11-01 08:37:53 -0300

    Fixing Heroku issues

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,7 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: "AWS",
-    aws_access_key_id: "AKIAIA5GE5E7QJQCGAAA"
+    aws_access_key_id: "AKIAIA5GE5E7QJQCGAAA",
     aws_secret_access_key: "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"
   }
   config.fog_directory = "instanap"
