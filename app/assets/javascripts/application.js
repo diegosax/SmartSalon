0f585d7555d5fa1b4d1119aea25653534028b4d7
Diego Marcolan <diegosax@terra.com.br>
2012-11-08 09:46:01 -0300

    Login Page fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require bootstrap.min
 //= require jquery-ui
+//= require jquery.actual.min
 //= require datatables/jquery.dataTables.min
 //= require datatables/TableTools/js/TableTools.min
 //= require datatables/dataTables.bootstrap
